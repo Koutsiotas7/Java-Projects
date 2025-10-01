2cf5d18f774e2d7be5b3bca17a22cb4d77ece8ab
Koutsi <ckoutsiotas@gmail.com>
2025-10-01 15:14:12 +0300

    Removed unnecessary projects

--- learn-spring-boot/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO COURSE(ID,AUTHOR,NAME) values (100001,'in28minutes','Learn Spring');
-INSERT INTO COURSE(ID,AUTHOR,NAME) values (100002,'in28minutes','Learn JPA');
-INSERT INTO COURSE(ID,AUTHOR,NAME) values (100003,'in28minutes','Learn Fullstack');
